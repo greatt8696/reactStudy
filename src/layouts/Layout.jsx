080ff4e4385514ecb959efb2360474c57850aa91
greatt8696 <88331836+greatt8696@users.noreply.github.com>
2023-01-02 10:25:10 +0900

    init component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,19 +1,18 @@
-import { Sidebar } from 'lucide-react'
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import SideBarView from '../components/sideBar/SideBarView'
-import TopBarView from '../components/topBar/TopBarView'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import SideBar from "../components/sideBar";
+import TopBar from "../components/topBar";
 
 const Layout = () => {
   return (
     <div className="w-screen h-screen flex flex-col bg-green-300">
       <Notification />
-      <TopBarView />
-      <SideBarView />
+      <TopBar />
+      <SideBar />
       {/* Outlet 안에 페이지들이 들어갑니다. */}
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
